943c218c7313e9754253a39ca93177c32edcd255
David Njuguna <dnjuguna@gmail.com>
2014-08-29 11:38:36 +0300

    initial commit

---
+++ wepesi-zopim.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: Wepesi Zopim
+Plugin URI: http://wepesi.com
+Description: Wepesi Zopim.
+Author: David Njuguna
+Version: 0.1
+Author URI: http://davidnjuguna.com
+*/
+
+add_action('admin_enqueue_scripts', 'wepesi_zopim_script');
+function wepesi_zopim_script() {
+    wp_enqueue_style('wepesi-zopim-script', plugins_url('js/zopim.js', __FILE__));
+}
+
