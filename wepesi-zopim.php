0959264aef1da7b50809e2fdaa2f6e82f0f03553
David Njuguna <dnjuguna@gmail.com>
2014-08-29 11:43:59 +0300

    fixed bug

--- wepesi-zopim.php
+++ wepesi-zopim.php
@@ -11,6 +11,6 @@ Author URI: http://davidnjuguna.com
 
 add_action('admin_enqueue_scripts', 'wepesi_zopim_script');
 function wepesi_zopim_script() {
-    wp_enqueue_style('wepesi-zopim-script', plugins_url('js/zopim.js', __FILE__));
+    wp_enqueue_script('wepesi-zopim-script', plugins_url('js/zopim.js', __FILE__));
 }
 
